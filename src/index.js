075361fffc4a7805724e1450487ed7b975e2626f
sfujimoto <unicoshun@gmail.com>
2016-10-31 22:31:47 +0900

    add react-route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+import App from './components/App';
+import Header from './components/Header';
+import Login from './components/Login';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={Header}>
+      <Route path="tests" component={App}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
